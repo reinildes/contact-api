132d589b53de9b00e2a16ffe8399861e83bab593
Ray Oliveira <reinildes.jmo@gmail.com>
2020-08-21 20:10:13 -0300

    add test for minors

--- test/Contact.js
+++ test/Contact.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const Contact = require("../src/Contact")
+const expect = require('chai').expect;
 
 describe('Contact', function () {
   describe('#getAge()', function () {
@@ -7,17 +8,16 @@ describe('Contact', function () {
 
         let mary = new Contact("Mary Lucia", "F", new Date(1991, 11, 8))
 
-      assert.equal(mary.getAge(), 28);
+        assert.equal(mary.getAge(), 28);
+
     });
   });
 
   describe('#getAge()', function () {
     it("Throws Error when Contact is a minor", function () {
-
  
-        assert.throws(() => new Contact("Juninho", "M", new Date(2003, 11, 8)), Error, "Error: Not safe for minors");
+        expect(() => new Contact("Juninho", "M", new Date(2003, 11, 8))).to.throw("Not safe for minors!")
       
-
-    });
+      });
   });
 });
