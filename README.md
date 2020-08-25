801ee6ba8e766846231ecec18132dec2684a756c
Ray Oliveira <reinildes.jmo@gmail.com>
2020-08-25 20:51:55 -0300

    add seed method to populate some test data

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ MONGO_URL='mongodb://localhost/test?retryWrites=true&w=majority' npm start
 
 ### Test with curl
 ````
-curl -X POST -d "{\"name\": \"Maria Joaquina\", \"gender\": \"F\", \"birthDate\": \"2000-08-25T01:59:30.046Z\"}" -H "Content-Type: application/json" http://localhost:3000/contact -ki
+curl -X POST -d "{\"name\": \"Maria Joaquina\", \"gender\": \"F\", \"birthDate\": \"2000-08-25T01:59:30.046Z\"}" -H "Content-Type: application/json" http://localhost:3000/contact -ki ;
 
 curl -X PUT -d "{\"name\": \"Maria Belarmina\", \"gender\": \"F\", \"birthDate\": \"2000-08-25T01:59:30.046Z\"}" -H "Content-Type: application/json" http://localhost:3000/contact/5f4478fd8d3ec01ec6249267 -ki
 
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 repository.connect()
+    .then(() => seed())
 
 app.get("/", (req, res) => {
     res.send("Welcome to contact api")
@@ -42,5 +43,18 @@ app.put("/contact/:id", async (req, res) => {
         res.status(500).send(error.message)        
     }
 })
+
+const seed = () => {
+
+    const contacts = [
+        new Contact("Luiz Inacio Lula da Silva", "M", new Date(1950, 10, 17)),
+        new Contact("Jair Messias Bolsolaro", "M", new Date(1960, 8, 13)),
+        new Contact("Dilma Rousseff", "F", new Date(1970, 2, 13)),
+        new Contact("Fernando Henrique Cardoso", "M", new Date(1940, 4, 5)),
+        new Contact("Fernando Color", "M", new Date(1958, 10, 1))
+    ]
+
+    contacts.forEach( contact => repository.addContact(contact))
+}
 
 app.listen(3000, () => console.log("Server is running!"))
